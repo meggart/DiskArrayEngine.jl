62e6c5aafd5b539478a9f731c04d3dad0e88dbbb
Fabian Gans <fgans@bgc-jena.mpg.de>
2023-05-09 16:28:08 +0200

    continue work on enginearrays

--- devscripts/arrays.jl
+++ devscripts/arrays.jl
@@ -12,8 +12,8 @@ DiskArrays.writeblock!(a::EngineArray,xout,r::OrdinalRange...) = DiskArrays.writ
 Base.size(a::EngineArray) = size(a.parent)
 DiskArrays.eachchunk(a::EngineArray) = DiskArrays.eachchunk(a.parent)
 
-using NetCDF
-a = NetCDF.open("../../../Documents/data/C3S-LC-L4-LCCS-Map-300m-P1Y-2020-v2.1.1.nc","lccs_class")
+using Zarr
+a = zopen("/home/fgans/data/esdc-8d-0.25deg-184x90x90-2.1.1.zarr/air_temperature_2m/")
 
 ae = EngineArray(a)
 
@@ -36,10 +36,12 @@ function Base.mapreduce(f, op, A::EngineArray...; dims=:, init = nothing)
     ia = map(A) do ar
         InputArray(ar.parent,dimsmap = ar.bcdims[])
     end
-    tf = Core.Compiler.return_type(f,Tuple{eltype.(A)...})
-
-    top = Core.Compiler.return_type(op,Tuple{tf})
-    func = create_userfunction(f,top,red=op,init=init,buftype=tf)
+    tf = Core.Compiler.return_type(f,Tuple{Base.nonmissingtype.(eltype.(A))...})
+    top = Core.Compiler.return_type(op,Tuple{tf,tf})
+    if any(a->eltype(a) >: Missing,A) 
+        top = Union{top,Missing}
+    end
+    func = create_userfunction(f,top,red=op,init=init,buftype=top)
     if dims === Colon()
         dims = ntuple(identity,nd)
     end
@@ -51,8 +53,45 @@ function Base.mapreduce(f, op, A::EngineArray...; dims=:, init = nothing)
     first(results_as_diskarrays(op))
 end
 using Statistics
-Base.maximum(a::EngineArray;dims=:) = maximum(identity,a;dims)
-Base.maximum(f::Base.Callable,a::EngineArray)
-r = mapreduce(identity,min,ae,init=0xff,dims=1)
+missred(f) = (a,b)-> ismissing(a) ? b : ismissing(b) ? a : f(a,b)
+init_min(a) = typemax(Base.nonmissingtype(eltype(a)))
+init_max(a) = typemin(Base.nonmissingtype(eltype(a)))
+for (f,red,i) in (
+    (:(Base.maximum),:max,:init_max),
+    (:(Base.minimum),:min,:init_min),
+    (:(Base.extrema),)
+)
+    eval(quote
+        $f(a::EngineArray;dims=:,skipmissing=false) = $f(identity,a;dims,skipmissing)
+        function $f(ff::Base.Callable,a::EngineArray;dims=:,skipmissing=false)
+            red = skipmissing ? missred($red) : $red
+            init = skipmissing ? missing : $i
+            mapreduce(ff,red,a;dims,init=$(i)(a))
+        end 
 
-r[32000,1]
+
+    end)
+end
+
+
+function compute(a::DiskArrayEngine.GMWOPResult)
+
+    ret = zeros(eltype(a),size(a))
+    lr = DiskArrayEngine.optimize_loopranges(a.op,1e9,tol_low=0.2,tol_high=0.05,max_order=2)
+    outars = ntuple(i->Val(i)==a.ires ? ret : nothing,length(a.op.outspecs))
+    r = DiskArrayEngine.LocalRunner(a.op,lr,outars,threaded=true)
+    run(r)
+    ret
+end
+
+r = minimum(ae,dims=3,skipmissing=true)
+
+@time r[1000,720]
+
+
+@time aa = compute(r)
+
+using Plots
+heatmap(reverse(permutedims(aa),dims=1))
+
+
