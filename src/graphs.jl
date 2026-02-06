194b5746954f08aea423aee015255d0622253dba
Fabian Gans <fgans@bgc-jena.mpg.de>
2026-02-06 15:10:38 +0000

    Make node list in graph a dict for much easier graph modification

--- src/graphs.jl
+++ src/graphs.jl
@@ -29,7 +29,7 @@ mutable struct MwopGraph <: AbstractGraph{Int}
     nodes
     connections
 end
-MwopGraph() = MwopGraph(1:0, [], [])
+MwopGraph() = MwopGraph(1:0, Dict{Int,Any}(), [])
 Base.zero(::Type{MwopGraph}) = MwopGraph()
 function Graphs.edges(g::MwopGraph)
     edges = Edge[]
@@ -66,11 +66,11 @@ function get_edge(g::MwopGraph, s, d)
     g.connections[iconn], r[iconn]...
 
 end
-Graphs.has_vertex(g::MwopGraph, i) = 0 < i <= length(g.nodes)
+Graphs.has_vertex(g::MwopGraph, i) = haskey(g.nodes, i)
 Graphs.is_directed(::Type{<:MwopGraph}) = true
 Graphs.ne(g::MwopGraph) = sum(c -> length(c.inputids) * length(c.outputids), g.connections)
 Graphs.nv(g::MwopGraph) = length(g.nodes)
-Graphs.vertices(g::MwopGraph) = 1:length(g.nodes)
+Graphs.vertices(g::MwopGraph) = collect(keys(g.nodes))
 function inconnections(g, v)
     findall(c -> in(v, c.outputids), g.connections)
 end
@@ -99,22 +99,24 @@ function edgenames(g::MwopGraph)
     n
 end
 function nodenames(g::MwopGraph)
-    map(i -> describe(i[2], i[1]), enumerate(g.nodes))
+    map(i -> describe(i[2], i[1]), collect(g.nodes))
 end
 
 function add_node!(g::MwopGraph, a)
     id = findfirst(Base.Fix1(===, a), g.nodes)
     if id === nothing
-        push!(g.nodes, a)
-        id = length(g.nodes)
+        maxkey = isempty(g.nodes) ? 0 : maximum(keys(g.nodes))
+        g.nodes[maxkey+1] = a
+        id = maxkey + 1
     end
     id
 end
 add_node!(g::MwopGraph, inar::InputArray) = add_node!(g, inar.a)
 add_node!(g::MwopGraph, outspec::NamedTuple, et) = add_node!(g, MwopOutNode(outspec.ismem, outspec.chunks, map(windowmaximum, outspec.lw.windows.members), et))
 function add_node!(g::MwopGraph, n::MwopOutNode)
-    push!(g.nodes, n)
-    length(g.nodes)
+    maxkey = isempty(g.nodes) ? 0 : maximum(keys(g.nodes))
+    g.nodes[maxkey+1] = n
+    maxkey + 1
 end
 
 ## Two connections are seen as equivalent when they apply the same operation
@@ -126,46 +128,34 @@ function equivalent_conection(c1, c2)
         c1.outwindows == c2.outwindows
 end
 
-function merge_connection!(g, i, j)
+function merge_connection!(g, i, j, replacedict)
     #Reroute all outputs of j
     c1 = g.connections[i]
     c2 = g.connections[j]
     deleteat!(g.connections, j)
     for (o_new, o_old) in zip(c1.outputids, c2.outputids)
+        replacedict[o_old] = o_new
         for c in g.connections
             ii = findall(==(o_old), c.inputids)
             c.inputids[ii] .= o_new
         end
     end
     for del in c2.outputids
-        delete_node!(g, del)
-    end
-end
-function delete_node!(g, del)
-    deleteat!(g.nodes, del)
-    for c in g.connections
-        for i in 1:length(c.inputids)
-            if c.inputids[i] > del
-                c.inputids[i] = c.inputids[i] - 1
-            end
-        end
-        for i in 1:length(c.outputids)
-            if c.outputids[i] > del
-                c.outputids[i] = c.outputids[i] - 1
-            end
-        end
+        delete!(g.nodes, del)
     end
 end
 
-function remove_aliases!(g::MwopGraph)
+
+function remove_aliases!(g::MwopGraph, replacedict=Dict{Int,Int}())
     for i in 1:length(g.connections)
         for j in (i+1):length(g.connections)
             if equivalent_conection(g.connections[i], g.connections[j])
-                merge_connection!(g, i, j)
-                return remove_aliases!(g)
+                merge_connection!(g, i, j, replacedict)
+                return remove_aliases!(g, replacedict)
             end
         end
     end
+    return replacedict
 end
 
 
